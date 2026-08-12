d0eff3329de6541692b2a7d9f1ac842a7539487e
eerraa <phyojinbin@gmail.com>
2026-08-12 22:03:54 +0900

    Add minimal ERA fork identity to global menu

--- src/components/menus/external-links.tsx
+++ src/components/menus/external-links.tsx
@@ -1,36 +1,38 @@
-import {faDiscord, faGithub} from '@fortawesome/free-brands-svg-icons';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import styled from 'styled-components';
-import {VIALogo} from '../icons/via';
-import {CategoryMenuTooltip} from '../inputs/tooltip';
-import {CategoryIconContainer} from '../panes/grid';
+import {LanguageSelect} from './language-select';
 
 const ExternalLinkContainer = styled.span`
   position: absolute;
   right: 1em;
   display: flex;
+  align-items: center;
   gap: 1em;
+`;
+
+const EraMark = styled.span`
+  color: var(--color_inside-accent);
+  display: inline-flex;
+  align-items: center;
+  height: 25px;
+  opacity: 0.75;
+  user-select: none;
+  white-space: nowrap;
+`;
+
+const EraWordmark = styled.span`
+  font-family: GothamRoundedBold, 'Fira Sans Condensed', sans-serif;
+  font-size: 24px;
+  font-weight: 500;
+  letter-spacing: -0.08em;
+  line-height: 1;
+  transform: skewX(-8deg);
 `;
 
 export const ExternalLinks = () => (
   <ExternalLinkContainer>
-    <a href="https://caniusevia.com/" target="_blank">
-      <CategoryIconContainer>
-        <VIALogo height="25px" fill="currentColor" />
-        <CategoryMenuTooltip>Firmware + Docs</CategoryMenuTooltip>
-      </CategoryIconContainer>
-    </a>
-    <a href="https://discord.gg/NStTR5YaPB" target="_blank">
-      <CategoryIconContainer>
-        <FontAwesomeIcon size={'xl'} icon={faDiscord} />
-        <CategoryMenuTooltip>Discord</CategoryMenuTooltip>
-      </CategoryIconContainer>
-    </a>
-    <a href="https://github.com/the-via/app" target="_blank">
-      <CategoryIconContainer>
-        <FontAwesomeIcon size={'xl'} icon={faGithub} />
-        <CategoryMenuTooltip>Github</CategoryMenuTooltip>
-      </CategoryIconContainer>
-    </a>
+    <LanguageSelect />
+    <EraMark aria-label="ERA">
+      <EraWordmark aria-hidden="true">ERA</EraWordmark>
+    </EraMark>
   </ExternalLinkContainer>
 );
